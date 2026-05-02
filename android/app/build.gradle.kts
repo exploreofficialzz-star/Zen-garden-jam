17776c9053422f803c20d04551d5c55115a7e5e6
exploreofficialzz-star <260314585+exploreofficialzz-star@users.noreply.github.com>
2026-05-02 12:15:14 -0400

    Apply additional fixes from zen_garden_jam_fixes-1.zip

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,6 +22,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // Required by flutter_local_notifications for Java 8+ API support on older Android versions
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -77,4 +79,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Required for isCoreLibraryDesugaringEnabled (flutter_local_notifications)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
